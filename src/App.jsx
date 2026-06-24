04d668e418665c5bafe04b89b8d28b15e8bf22ca
taskeenkhan-bscs <taskoowzr@example.com>
2026-06-24 12:28:43 +0500

    Initial React project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white text-4xl font-bold h-screen flex items-center justify-center">
+      Tailwind CSS Working ✅
+    </div>
+  )
+}
+
+export default App
